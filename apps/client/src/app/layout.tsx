6df296c5b89cef6afbff58afed2a8ddc981737da
Jhon <eduardcamayo13@gmail.com>
2024-07-13 19:47:34 -0500

    Change: Background and text color

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,8 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={cn("bg-gradient-to-r from-[#1e0d35] to-[#0f101b]", inter.className)}>
+          <div className="fixed w-32 h-32 bg-[#8f3b76] blur-[110px] top-[30%] left-[20%]" ></div>
           {children}
           <Footer />
         </body>
